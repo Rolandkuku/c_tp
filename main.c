6b85839e56a79436e7faa6569e6cb2c23bc3d22f
Roland Kullet <renaud.bellec.3@gmail.com>
2016-05-31 14:23:40 +0200

    project basefile

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    printf("Toto");
 }
